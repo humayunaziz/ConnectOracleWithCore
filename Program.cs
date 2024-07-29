73c0ac18a692f9609a1bdb7a2ab16c73c4a2f8e2
humayun.aziz@live.com <humayunaziz2016@gmail.com>
2024-07-29 14:22:20 +0500

    add project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Oracle.ManagedDataAccess.Client;
+
+using (OracleConnection conn = new OracleConnection("data source=abc;password=bac;user id=cda;"))
+{
+    try
+    {
+        using (OracleCommand cmd = conn.CreateCommand())
+        {
+            conn.Open();
+            if (conn.State == System.Data.ConnectionState.Open)
+                Console.WriteLine("Connection opened successfully.");
+
+            cmd.CommandText = "select * from get_view";
+
+            using (OracleDataReader reader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection))
+            {
+                while (reader.Read())
+                    Console.WriteLine(reader.GetValue(1));
+            }
+        }
+    }
+    catch (OracleException ex) { Console.WriteLine($"Oracle error occurred: {ex.Message}"); }
+    catch (Exception ex) { Console.WriteLine($"An error occurred: {ex.Message}"); }
+}
+
+Console.ReadLine();
